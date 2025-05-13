b5f0857b062580c24ce14b7ae204349f63aab736
ibrahim <ibrahimjp630@gmail.com>
2025-05-13 21:16:43 +0530

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,36 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, createHashRouter, HashRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Shop from './pages/Shop.jsx';
-import Contact from './pages/Contact.jsx';
-import Blog from './pages/Blog.jsx';
-const router = createHashRouter([
-  {
-    path:'/',
-    element:<Home/>
-  },
-  {
-    path:'/about',
-    element:<About/>
-  },{
-    path:'/shop',
-    element:<Shop/>
-  },{
-    path:'/contact-us',
-    element:<Contact/>
-  },
-  {
-    path:'/blog',
-    element:<Blog/>
-  }
-])
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-      <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
