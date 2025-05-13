9f59b8cf99f39cdf9ab039e75b214fb8957b2971
ibrahim <ibrahimjp630@gmail.com>
2025-05-13 20:59:27 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
   plugins: [
     react(),
     tailwindcss(),
